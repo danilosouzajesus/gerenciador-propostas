ef50cfa6682908c9e1d09bf709f29c2ba15cc514
Danilo de Souza Jesus <danilosouzajesus@gmail.com>
2018-05-30 13:50:05 +0000

    adding firt provider class

---
+++ webapi/Data/Provider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gerenciador_propostas.Models
+{
+    public class Provider
+    {
+        public int providerId { get; set; }
+        public string name { get; set; }
+        public string nickname { get; set; }
+        public string cnpj { get; set; }
+        public string phone { get; set; }
+        public string contactName { get; set; }
+        public string address { get; set; }
+        public string addressNumber { get; set; }
+        public string cep { get; set; }
+        public string city { get; set; }
+        public string province { get; set; }
+        public string country { get; set; }
+        public DateTime lastUpdate { get; set; }
+    }
+}
